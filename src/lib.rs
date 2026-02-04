bbe61f24d83bdf48d57b835f70bf1a809b37b89a
martinbudden <mjbudden@gmail.com>
2026-02-04 22:58:00 +0000

    Published modules.

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,9 @@ mod complementary_filter;
 mod madgwick_filter;
 mod mahony_filter;
 mod sensor_fusion;
+
+pub use sensor_fusion::SensorFusion;
+pub use sensor_fusion::q_dot;
+pub use complementary_filter::ComplementaryFilter;
+pub use madgwick_filter::MadgwickFilter;
+pub use mahony_filter::MahonyFilter;
